3cc2daa229d26f349ed3537afc8d594bfe0e0bf0
aman-alem <alem.aman@gmail.com>
2023-03-18 16:32:32 -0400

    connection.js created

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const monoURI = process.env.REACT_APP_DB_URL;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+  })
+  .then((instance) => {
+    console.log(`connected on ${instance.connections[0].name}`);
+  })
+  .catch((err) => console.log(`ERROR: See details, ${err}`));
