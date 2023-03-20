27d8d907b382802c042bef3e14fc82748292967d
aman-alem <alem.aman@gmail.com>
2023-03-20 16:59:35 -0400

    word create function

--- controllers/wordCtrl.js
+++ controllers/wordCtrl.js
@@ -5,9 +5,26 @@ const index = (req, res) => {};
 const show = (req, res) => {};
 
 const create = async (req, res) => {
-  let newWord = await Word.create(req.body);
+  if (req.body.word.length !== 5) {
+    return res.json("Word must be exactly 5 letters long. Please try again.");
+  } else {
+    let word = await req.body.word.toLowercase();
+    let url = await `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
-  res.json(newWord);
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.title) {
+          return res.json(
+            "Word must be in the english dictionary! Please try again"
+          );
+        } else {
+          let newWord = Word.create(req.body);
+
+          return res.json(newWord.word);
+        }
+      });
+  }
 };
 
 const update = (req, res) => {};
